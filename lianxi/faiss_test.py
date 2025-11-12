5206d617d71b81a95eb5de4fa43e1c53f0acecce
yguo <lkjx031021@163.com>
2025-11-12 15:27:34 +0800

    add test

--- lianxi/faiss_test.py
+++ lianxi/faiss_test.py
@@ -1,5 +1,7 @@
 import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores import Milvus
 from langchain_ollama import OllamaEmbeddings, ChatOllama
 from langchain.schema import Document
 
@@ -19,11 +21,25 @@ if not kb_path.exists():
 
 def create_new_kb():
     embed_model = OllamaEmbeddings(model="bge-m3:latest")
-    if os.path.exists(kb_path):
+    if os.path.exists(Path(kb_path) / "index.faiss"):
         vector_store = FAISS.load_local(kb_path, embed_model, distance_strategy="METRIC_INNER_PRODUCT", allow_dangerous_deserialization=True)
+        print("load local")
         return vector_store
     doc = Document(page_content="init", metadata={})
     vector_store = FAISS.from_documents([doc], embed_model, distance_strategy="METRIC_INNER_PRODUCT")
+    ids = list(vector_store.docstore._dict.keys())
+    vector_store.delete(ids)
+    vector_store.save_local(kb_path)
+    return vector_store
+
+def create_new_kb_milvus():
+    embed_model = OllamaEmbeddings(model="bge-m3:latest")
+    if os.path.exists(Path(kb_path) / "index.milvus"):
+        vector_store = Milvus.load_local(kb_path, embed_model, distance_strategy="METRIC_INNER_PRODUCT", allow_dangerous_deserialization=True)
+        print("load local")
+        return vector_store
+    doc = Document(page_content="init", metadata={})
+    vector_store = Milvus.from_documents([doc], embed_model, distance_strategy="METRIC_INNER_PRODUCT")
     ids = list(vector_store.docstore._dict.keys())
     vector_store.delete(ids)
     vector_store.save_local(kb_path)
@@ -40,21 +56,34 @@ def embed(query):
     query_vector = embed_model.embed_query(query)
     return query_vector
 
+def text_split(docs):
+    from langchain_text_splitters import RecursiveCharacterTextSplitter as rcs
+
+    text_splitter = rcs(chunk_size=500, chunk_overlap=100)
+    return text_splitter.split_documents(docs)
+
 if __name__ == "__main__":
     # model = ChatOllama(model="qwen3:4b")
     # print(model.invoke("数学有什么用"))
     # exit()
-    docs = load_docs("lianxi/README.md")
-    text = [x.page_content for x in docs]
-    embeddings = OllamaEmbeddings(model="bge-m3:latest").embed_documents(text)
-    print(embeddings)
-    print(len(embeddings))
-    exit()
-    vs = create_new_kb()
-    vs.add_embeddings()
-    res = vs.similarity_search_by_vector(embed("RAG增强可以使用的框架？"), k=3)
-    print(docs[0])
-    print(len(docs))
+    # docs = load_docs("lianxi/README.md")
+    # texts = text_split(docs)
+    # text = [x.page_content for x in texts]
+    # embeddings = OllamaEmbeddings(model="bge-m3:latest").embed_documents(text)
 
-    print(type(docs[0]))
+    vs = create_new_kb()
+    # print(vs)
+    # print(vs.docstore._dict)
+    # print(len(vs.docstore._dict))
+    # vs.add_embeddings(zip(text, embeddings))
+    # vs.save_local(kb_path)
+    # query = embed("RAG增强可以使用的框架？")
+    query = embed("RAG索引算法有哪些")
+    res = vs.similarity_search_by_vector(query, k=3)
+    # print(docs[0])
+    # print(len(docs))
+
+    # print(type(docs[0]))
     print(res)
+    # for i in res:
+    #     print(i) 
